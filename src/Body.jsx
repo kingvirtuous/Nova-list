6be30d6416b786843259a1682a70f6217c850d1a
kingvirtuous <kingvirtuous08@gmail.com>
2026-09-17 22:04:37 +0000

    done for today

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body Component</div>
+    <div>
+      <div className="p-3 items-center m-3 flex justify-center">
+        <input placeholder="What need to be done?" className="p-2 w-[500px] h-[50px]" />
+        <button className="bg-blue-500 p-4 rounded-lg ml-3.5">
+          <i className="fa-solid fa-plus text-3xl text-white"></i>
+        </button>
+      </div>
+      
+      
+    </div>
   )
 }
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className="bg-linear-to-r from-blue-600 to-blue-300 justify-between flex px-5 py-10 items-center rounded-b-[40px]">
+      <div className="text-white font-extrabold ml-2 text-4xl">Nova-List<i className="fa-solid fa-circle-check text-3xl ml-2.5 text-white"></i></div>
+      <div><i className="fa-solid fa-sun text-yellow-500 mr-2 text-3xl"></i></div>
+    </div>
   )
 }
 
